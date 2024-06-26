e667495b7455cdb7d01f6a68894a5ffc894f4d87
Autophile-Dev <waleed.zaheer55@gmail.com>
2024-06-26 23:14:43 +0800

    Reinitialize the backend index,js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const MONGO_URI = "mongodb+srv://waleedzaheer55:mORxmKA29Cu2K3CI@learning-mern.c
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const userRoute = require('./routes/users');
+const userRoute = require('./src/routes/users');
 
 // Middleware to parse incoming or outgoing JSON format data
 app.use(cors());
